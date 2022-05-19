7901895debd0dfb789b690599f577f6ac6970c30
tohidshabanloo <tshabanloo@hotmail.com>
2022-05-19 11:29:23 +0430

    rename components

--- src/components/Panel/Panel.js
+++ src/components/Panel/Panel.js
@@ -1,9 +1,14 @@
 import React from "react";
+import Dashboard from "../Dashboard/Dashboard";
+import VisualComponents from "../VisualComponents/VisualComponents";
+import Inspector from "../Inspector/Inspector";
 
 const Panel = () => {
   return (
     <>
-      
+      <Inspector />
+      <Dashboard />
+      <VisualComponents />
     </>
   );
 };
